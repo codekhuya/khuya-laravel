801e81d856f91e7311ac77f32a478ec239e9bd5f
Thanh Huynh <codekhuya@gmail.com>
2018-07-15 12:20:24 +0700

    initial commit

---
+++ promotion/app/Http/Controllers/PromotionCodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PromotionCode;
+use Illuminate\Http\Request;
+
+class PromotionCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PromotionCode  $promotionCode
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PromotionCode $promotionCode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PromotionCode  $promotionCode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PromotionCode $promotionCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PromotionCode  $promotionCode
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PromotionCode $promotionCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PromotionCode  $promotionCode
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PromotionCode $promotionCode)
+    {
+        //
+    }
+}
---
+++ promotion/database/factories/PromotionCodeFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PromotionCode::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
