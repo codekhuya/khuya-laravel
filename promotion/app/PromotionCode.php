0d9438337904f3e6af4a66c98d31a5067cce13eb
Thanh Huynh <codekhuya@gmail.com>
2018-07-16 17:25:36 +0700

    Ham generate code unique

--- promotion/app/PromotionCode.php
+++ promotion/app/PromotionCode.php
@@ -7,7 +7,20 @@ use App\Promotion;
 
 class PromotionCode extends Model
 {
+    protected $fillable = ['code', 'actived', 'value', 'type'];
+    
     public function promotion(){
         return $this->belongsTo(Promotion::class);
+    }
+
+    public static function codeGenerate($length = 9)
+    {
+        $pool = strtoupper(str_random($length));
+        $existedCode = PromotionCode::where('code',$pool)->first();
+        if($existedCode){
+            //Neu trung thi goi lai
+            return $pool = PromotionCode::codeGenerate($length);
+        }
+        return $pool;
     }
 }
