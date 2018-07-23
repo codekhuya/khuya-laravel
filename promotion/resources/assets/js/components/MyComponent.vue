46ffb2598cba9f0a64e84185e86ba1cbe5aba96f
Thanh Huynh <codekhuya@gmail.com>
2018-07-23 09:42:02 +0700

    cài thêm Vue.js

---
+++ promotion/resources/assets/js/components/MyComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="my-component">
+        <div> {{ message.text }} </div>
+        <div><button @click="changeMessage">Click me</button></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: {
+                type: 'greeting',
+                text: 'How are you'
+            }
+        };
+    },
+    methods: {
+        changeType() {
+            this.message.type = "This is a NEW Type";
+        },
+        changeText() {
+            this.message.text = 'This is NEW Text'
+        }
+    },
+    computed: {
+        getType(){
+            return this.message.type
+        },
+        getText(){
+            return this.message.text
+        }
+    },
+    watch: {
+        getType() {
+            console.log('type changed')
+        },
+        getText(){
+            console.log('text changed')
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.my-component {
+    color: blueviolet;
+}
+</style>
